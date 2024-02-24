0dcf92bd058c9d76c207e51c8c11b9ac4fab153f
minjeek <kmj4138@gmail.com>
2024-02-24 15:36:15 +0900

    test
    develop 브랜치에서 변경사항 생성

--- notion-import.js
+++ notion-import.js
@@ -152,7 +152,7 @@ author: "${author}"
     );
 
     //writing to file
-    fs.writeFile(path.join(tags[0], ftitle), fm + edited_md, (err) => {
+    fs.writeFile(path.join("_posts", ftitle), fm + edited_md, (err) => {
       if (err) {
         console.log(err);
       }
