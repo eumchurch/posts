248b47df5cf07c532b25b40d1654080194e28a66
minjeek <kmj4138@gmail.com>
2024-03-01 16:04:45 +0900

    파일 저장로직 추가

--- youtube-import.js
+++ youtube-import.js
@@ -1,17 +1,14 @@
+const fs = require("fs");
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 const PLAYLIST_ID_SERMON = "PLzCVCPy03Qq1ySW_mrIsXdrrDw35NBRyE";
 const PLAYLIST_ID_QT = "PLzCVCPy03Qq2itb1HzvL5CV-TwTCRfFRA";
+
 const TIMEZONE_OFFSET = 1000 * 60 * 60 * 9;
 const A_DAY_OFFSET = 1000 * 60 * 60 * 24;
 
 const baseurl = 'https://www.googleapis.com/youtube/v3/playlistItems';
-const params = {
-    key: GOOGLE_API_KEY,
-    playlistId: PLAYLIST_ID_SERMON,
-    maxResults: 20,
-    part: "snippet"
-};
+
 
 function convertPostDate(publishedAt) {
   let publishedDate = new Date(publishedAt);
@@ -30,16 +27,41 @@ function convertPostDate(publishedAt) {
   return year + "-" + month + "-" + date;
 }
 
-const queryString = new URLSearchParams(params).toString();  // url에 쓰기 적합한 querySting으로 return 해준다. 
-const requrl = `${baseurl}?${queryString}`;
+function createMD(date, title, subtitle, category, youtube, description) {
 
-const getData = async() => {
+  const fm = `---
+layout: post
+date: ${date}
+title: "${title}"
+category: "${category}"
+subtitle: "${subtitle}"
+youtube: "${youtube}"
+---
+
+`;
+  return fm + description;
+}
+
+(async () => {
+  // 주일설교
+  let category = "sermon"
+  fs.mkdirSync(category, { recursive: true });
+
+  const params = {
+      key: GOOGLE_API_KEY,
+      playlistId: PLAYLIST_ID_SERMON,
+      maxResults: 20,
+      part: "snippet"
+  };
+  const queryString = new URLSearchParams(params).toString();  // url에 쓰기 적합한 querySting으로 return 해준다. 
+  const requrl = `${baseurl}?${queryString}`; 
+
   const res = await fetch(requrl);
   const data = await res.json();
 
   let items = data?.["items"];
+  
   if (items) {
-    console.log("items ok");
     for (const item of items) {
       let snippet = item?.["snippet"];
       if (snippet) {
@@ -49,20 +71,32 @@ const getData = async() => {
         if (publishedAt) {
           date = convertPostDate(publishedAt);
         }
-        let videoId = snippet?.["resourceId"]?.["videoId"];
-        console.log("  videoId: " + videoId + ", date: " + date);
+        let youtube = snippet?.["resourceId"]?.["videoId"];
 
+
+        let title = "";
+        let subtitle = "";
         let description = snippet?.["description"];
         let array = description.split("\n\n");
         if (array.length == 3) {
-          console.log("  title: " + array[1] + ", subtitle: " + array[0]);
+          title = array[1];
+          subtitle = array[0];
+          description = array[2];
 
         } else {
           throw new Error("An error occured parsing youtube description.");
         }
+
+        let md = createMD(date, title, subtitle, category, youtube, description);
+
+        //writing to file
+        fs.writeFile(path.join(category, date + ".md"), md, (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
       }
     }
   }
-};
 
-getData();
+})();
