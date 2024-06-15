6d9978e6e0ba861cb05fe106077699154ac7f3d2
minjeek <kmj4138@gmail.com>
2024-06-15 23:51:10 +0900

    Update notion-import.js

--- notion-import.js
+++ notion-import.js
@@ -157,7 +157,6 @@ thumbnail: "${thumbnail}"`;
           console.log(p1);
           console.log(p2);
           console.log(p3);
-          console.log(p4);
         
         let filename = "/posts/" + saveImage(ftitle, index, p2);
         
